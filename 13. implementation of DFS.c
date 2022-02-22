1ba8d34f9b5a4960fe6aabfd63c07baeb949f1ab
Nithish Kumar <nithssh@protonmail.com>
2022-02-22 22:51:06 +0530

    Slightly improve DFS

--- 13. implementation of DFS.c
+++ 13. implementation of DFS.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
-void DFS(int);
-// n is no of vertices and graph is sorted in array G[10][10]
+
 int G[10][10], visited[10], n;
+
+void DFS(int i)
+{
+    printf("\n%d", i);
+    visited[i] = 1;
+    for (int j = 0; j < n; j++)
+        if (!visited[j] && G[i][j] == 1)
+            DFS(j);
+}
+
 void main()
 {
     int i, j;
     printf("Enter number of vertices:");
     scanf("%d", &n);
     // read the adjecency matrix
-    printf("\nEnter adjecency matrix of the graph : \n");
+    printf("\nEnter adjecency matrix of the graph:\n");
     for (i = 0; i < n; i++)
         for (j = 0; j < n; j++)
             scanf("%d", &G[i][j]);
@@ -16,14 +25,4 @@ void main()
     for (i = 0; i < n; i++)
         visited[i] = 0;
     DFS(0);
-}
-
-void DFS(int i)
-{
-    int j;
-    printf("\n%d", i);
-    visited[i] = 1;
-    for (j = 0; j < n; j++)
-        if (!visited[j] && G[i][j] == 1)
-            DFS(j);
 }
